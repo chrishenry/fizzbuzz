1c9edd4bdf3819636971425833199337b0705a5d
Chris Henry <henry.christopher@gmail.com>
2015-06-20 12:00:50 -0700

    Complete rust implementation of fizzbuzz.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,22 @@
 fn main() {
     for x in 1..101 {
 
-        let y = x % 3;
+        let fizz = x % 3;
+        let buzz = x % 5;
 
-        if y == 0 {
-            println!("fizz");
+        if fizz == 0 && buzz == 0 {
+            println!("FizzBuzz");
+            continue;
+        }
+
+        if fizz == 0 {
+            println!("Fizz");
+            continue;
+        }
+
+        if buzz == 0 {
+            println!("Buzz");
+            continue;
         }
 
         println!("{}", x);
